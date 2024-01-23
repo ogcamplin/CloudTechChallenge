e22bd6c5458f8f4c8d570c36010b984785ed5270
Oscar Camplin <Oscar.Camplin@airnz.co.nz>
2024-01-23 16:19:11 +1300

    MVP working version of ECS cluster

--- infra/modules/security_groups/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "security_groups" {
-  value = {
-    application = aws_security_group.application.id
-    alb = aws_security_group.alb.id
-    database = aws_security_group.database.id
-  }
-}
